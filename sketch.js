a8563b24695bc276eeedc0c3b4ca22f20872876e
CODE9901 <89019056+CODE9901@users.noreply.github.com>
2021-08-24 16:14:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if (keyDown(UP_ARROW))
+background('green')
+if(keyDown(DOWN_ARROW))
+background('red')
+if(keyDown(LEFT_ARROW))
+background('cyan')
+if(keyDown(RIGHT_ARROW))
+background('purple')
+
+}
+
+
+
+
